09985eeea66794db7f123890b581fc803f782baa
Projects-and-Stuff <support@projectsandstuff.com>
2010-10-30 19:22:14 -0400

    Updated with GPL 3.0 license header

--- Editor/mainwindow.h
+++ Editor/mainwindow.h
@@ -1,7 +1,34 @@
+/*
+    BlazeShow Editor - Modular LED Animations
+    Copyright 2010 Projects and Stuff, LLC
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+    Projects and Stuff, LLC
+    Website: http://www.projectsandstuff.com
+    Support: support@projectsandstuff.com
+*/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QtGui/QMainWindow>
+
+
+
+
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +37,18 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+
+
+
+private:
+
+
+
+
+
+
 };
 
 #endif // MAINWINDOW_H
